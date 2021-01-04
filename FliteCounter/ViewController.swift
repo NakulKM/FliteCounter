588c2724a46da02ee9b00466680560b109da4d9b
Nakul Mehta <nakulkmehta@gmail.com>
2021-01-04 04:02:39 -0600

    Initial Commit

---
+++ FliteCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FliteCounter
+//
+//  Created by Nakul Mehta on 1/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
